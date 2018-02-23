8526c0a6870cfc27198149f4c4dba2b571ee9f8e
floris09 <florismeininger@gmail.com>
2018-02-23 21:57:33 +0100

    change root yet again

--- private/initialize.php
+++ private/initialize.php
@@ -4,7 +4,7 @@
   define("PROJECT_PATH", dirname(PRIVATE_PATH));
   define("PUBLIC_PATH", PROJECT_PATH . '/public');
   define("SHARED_PATH", PRIVATE_PATH . '/shared');
-  define("WWW_ROOT", '/RSVP/public');
+  define("WWW_ROOT", '');
 
   require_once('functions.php');
 
