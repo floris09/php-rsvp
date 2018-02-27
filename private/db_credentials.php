f3bf0e20d09d1ae82e837de879dff4cf9916a26a
floris09 <florismeininger@gmail.com>
2018-02-27 22:41:18 +0100

    test

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
 
-define("DB_SERVER", "blv.herokuapp.com");
+define("DB_SERVER", "127.0.0.1");
 define("DB_USER", "balieveplanner");
 define("DB_PASS", "supercomplexpassword");
 define("DB_NAME", "blv_rsvp");
