887a955032b28721d2e7339c3554e8b05e5de190
floris09 <florismeininger@gmail.com>
2018-03-03 20:17:49 +0100

    add responsiveness

--- private/shared/header.php
+++ private/shared/header.php
@@ -14,7 +14,8 @@
 <head>
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel='stylesheet' href=<?= WWW_ROOT . '/styles/style.css?'.time(); ?> />
+  <link rel='stylesheet' href=<?= WWW_ROOT . '/styles/style.css?' ?> />
+  <link rel="stylesheet" media="screen and (min-width: 900px)" href=<?= WWW_ROOT . "/styles/widestyle.css" ?> >
   <title>Bali Eve RSVP</title>
 
 </head>
